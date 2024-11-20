d3f555d8027c03f26c3d8a7e8a09ef5aa42a17e3
Bagus <113045284+bagusws17@users.noreply.github.com>
2024-11-20 22:19:32 +0700

    Pertemuan 10

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAX_1Jl30GX9EJ6RSn-bvYwZYhnibgUsuw",
+    authDomain: "vue-firebase-c280a.firebaseapp.com",
+    projectId: "vue-firebase-c280a",
+    storageBucket: "vue-firebase-c280a.firebasestorage.app",
+    messagingSenderId: "182879177582",
+    appId: "1:182879177582:web:1ef61ab46f97a7d0fb8f7f"
+  };
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
